b163332d274f471d7bcc83d93e4560f06120fc8f
Matt Powel <powelmj@gmail.com>
2024-12-03 07:08:51 -0700

    3DEC ds

--- starter/src/BookShelf.js
+++ starter/src/BookShelf.js
@@ -1,19 +1,27 @@
-function BookShelf({title}){
+import Book from "./Book"
 
-
-
+function BookShelf({title,books,updateBookShelf}){
     return(
-
-
-
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map((book)=> (
+                        <Book key={book.id} book={book} updateBookShelf={updateBookShelf}/>
+                    ))} 
+                </ol>
+            </div>
+        </div>
     );
 
+};
 
-
-
-
-
-
-
-}
 export default BookShelf;
+
+
+
+
+
+
+
+
